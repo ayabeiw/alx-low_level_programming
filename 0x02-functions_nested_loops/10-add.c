24b6f28b71b1af2b5b61c1762df47680465873fa
William Ayabei <ayabeiw@gmail.com>
2021-06-22 13:45:45 -0700

    Second Submission

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * main - function that adds two integers and
+ *
+ * 
+ */
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - Computes the absolute value of an interger
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
