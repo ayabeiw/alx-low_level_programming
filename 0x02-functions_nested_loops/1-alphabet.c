4697682f527b0743d048637130b8ce53fbb51b85
William Ayabei <ayabeiw@gmail.com>
2021-06-22 22:19:35 -0700

    second try

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "holberton.h"
+
 /**
- * main - Print Holberton
+ * print_alphabet_x10 - Print alphabet in lower case
  *
  * Return: Always 0 (Success)
  */
