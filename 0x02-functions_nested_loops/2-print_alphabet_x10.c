706a317c61a6796eaa005c680467f48273dfe232
William Ayabei <ayabeiw@gmail.com>
2021-06-22 23:20:51 -0700

    Bettty Coding Style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,15 @@ void print_alphabet_x10(void)
 {
 int i = 0;
     
-while (i = 10)
+while (i++ = 10)
 
 {   
 char ch;
 for(ch = 'a' ; ch <= 'z' ; ch++) 
 
-{
+
 _putchar(ch);
 
-}
-
-i++;
 _putchar('\n');
 }    
 return 0;
