6317f2260b65299585c47a36c280ba8cc67d7b21
William Ayabei <ayabeiw@gmail.com>
2021-06-22 23:25:26 -0700

    Bettty Coding Style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
 
-
+/**
+ * print_alphabet_x10 - Prints 10 the alphabet ten times in lower case
+ */
 void print_alphabet_x10(void)
+
 {
-int i = 0;
-    
-while (i++ = 10)
+  int i = 0;
+  char ch;
 
-{   
-char ch;
-for(ch = 'a' ; ch <= 'z' ; ch++) 
-
-
-_putchar(ch);
-
-_putchar('\n');
-}    
-return 0;
+  while (i++ = 10)
+    {
+      for (ch = 'a'; letter <= 'z'; ch++)
+	_putchar(ch);
+      _putchar('\n');
+    }
 }
