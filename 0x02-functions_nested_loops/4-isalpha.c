d91ff1abe99e042273666a1bb577f9aaa14261a9
William Ayabei <ayabeiw@gmail.com>
2021-06-22 23:06:41 -0700

    Bettty Coding Style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,16 @@
 int _isalpha(int c)
 
 /**
-* main - Checks for alphabectic Characters
-*
+* _isalpha - Checks for alphabectic Characters
+*@c: letter to be chcked, lowercase or uppercase
 * Return: Always 0 (Success)
 */
 
-int _isalpha(int c)
-{
-  if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-    return (1);
-  else
-    return (0);
+int _isalpha(int c);
+
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+
+{return (1);
+else
+return (0);
 }
