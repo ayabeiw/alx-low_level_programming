b65c1be9f1a2dac1422dfd63c0439c4c33f191b4
William Ayabei <ayabeiw@gmail.com>
2021-06-22 22:31:40 -0700

    Modified 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,15 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet_x10(void);
-char ch;
+void print_alphabet_x10(void)
+
 {
+char ch;
+
 for(ch = 97 ; ch <= 122 ; ch++)
 _putchar(ch);
 
 
 _putchar('\n');
-return(0);
+
 }
